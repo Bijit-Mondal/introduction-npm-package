13cba096403e094aea0a2de2e1d6ec08c5a79b4c
Bijit-Mondal <bijitmondal3011@rediffmail.com>
2024-07-23 12:40:24 +0530

    Portfolio Link Changed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function logDetails() {
     const message = "Hola! I am Bijit Modal, I love to learn new technologies and solve problems using it. I am currently undergraduate student working on CodeDhyan(https://codedhyan.netlify.app) and SpotOn(https://spoton.ilib.website)";
     const githubLink = "https://github.com/Bijit-Mondal";
     const linkedinLink = "https://www.linkedin.com/in/bijit-mondal-3b196721b/";
-    const wesbiteLink = "https://codedhyan.netlify.app";
+    const wesbiteLink = "https://portfolio.bijit.xyz";
 
     // Create a colorful box using ANSI escape codes
     const colorfulBox = `\x1b[38;5;202m+---------------------------------------------------------------+
